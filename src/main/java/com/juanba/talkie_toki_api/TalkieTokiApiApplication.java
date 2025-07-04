61e27dffeb904863b0e2dfed3672fab1305fe850
JuanBarbosaMora <portelnom0308@gmail.com>
2025-07-04 12:02:45 -0500

    add first commit talkie tokie api

---
+++ src/main/java/com/juanba/talkie_toki_api/TalkieTokiApiApplication.java
@@ -0,0 +1,13 @@
+package com.juanba.talkie_toki_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkieTokiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkieTokiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanba/talkie_toki_api/TalkieTokiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanba.talkie_toki_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkieTokiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
