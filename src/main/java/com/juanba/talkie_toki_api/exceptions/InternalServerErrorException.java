45d422c5469a1cf03c868ed8738d6c2880b0e78d
JuanBarbosaMora <portelnom0308@gmail.com>
2025-07-05 14:30:54 -0500

    add error 500 exception

---
+++ src/main/java/com/juanba/talkie_toki_api/exceptions/InternalServerErrorException.java
@@ -0,0 +1,28 @@
+package com.juanba.talkie_toki_api.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class InternalServerErrorException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity internalServerError(Exception exception) {
+        return ResponseEntity.internalServerError().body(new ResponseInternalServerError("Server error 500-01", exception.getLocalizedMessage(), List.of("(Fake) Contact the SunnyBank Support Team at HelpDesk@sunnybank.com", "(Fake) International line 018000475763", "(Fake) Report the error at www.sunny-help-online-assessor.com")));
+    }
+
+    public record ResponseInternalServerError(
+            String typeError,
+            String message,
+            List<String> recommendations
+    ) {
+        public ResponseInternalServerError(String typeError, String message, List<String> recommendations) {
+            this.typeError = typeError;
+            this.message = message;
+            this.recommendations = recommendations;
+        }
+    }
+}
